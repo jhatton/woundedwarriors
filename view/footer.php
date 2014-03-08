71f87a3fc29374aac8e4a2e9ee5d9794784fd18b
Jim Hatton <jhatton@hotmail.com>
2014-03-08 15:46:02 -0600

    switching to MVC

---
+++ view/footer.php
@@ -0,0 +1,15 @@
+<footer>
+	<ul>
+    	<li><a href="index.html">Home</a></li>
+       <li><a href="about.html">About</a></li>
+    	<li><a href="store.html">Store</a></li>
+    	<li><a href="donate.html">Donate</a></li>
+    	<li><a href="subscribe.html">Subscribe</a></li>
+    	<li><a href="links.html">Links</a></li>
+	</ul>
+    <p>&copy; 2014 Wounded Warriors</p>
+</footer>
+<script type="application/javascript" src="js/jquery-1.10.2.min.js"></script>
+<script type="application/javascript" src="js/site.js"></script>
+</body>
+</html>
