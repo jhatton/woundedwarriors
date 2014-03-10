0ae1a9c33b8d3b32806c9e78a839cbe962b63c13
Jim Hatton <jhatton@hotmail.com>
2014-03-09 23:23:49 -0500

    copied footer and header to php files

--- view/footer.php
+++ view/footer.php
@@ -7,9 +7,12 @@
     	<li><a href="subscribe.html">Subscribe</a></li>
     	<li><a href="links.html">Links</a></li>
 	</ul>
-    <p>&copy; 2014 Wounded Warriors</p>
+   <br> <p>&copy; 2014 Wounded Warriors</p></br>
 </footer>
 <script type="application/javascript" src="js/jquery-1.10.2.min.js"></script>
 <script type="application/javascript" src="js/site.js"></script>
+<script type="text/javascript">
+swfobject.registerObject("FlashID");
+</script>
 </body>
 </html>
--- view/header.php
+++ view/header.php
@@ -3,5 +3,6 @@
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="css/main.css" />
-<title>Wounded Warriors: Home</title>
+<title>Home</title>
+<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
 </head>
