e554da809dee188a6d01ad784fd4999a24e09137
Jim Hatton <jhatton@hotmail.com>
2014-03-11 23:08:27 -0500

    added css to store page

--- js/site.js
+++ js/site.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
 
-$(".slideshow > div:gt(0)").hide();
-
-setInterval(function() {
-  $('.slideshow > div:first')
-    .fadeOut(2500)
-    .next()
-    .fadeIn(2500)
-    .end()
-    .appendTo('.slideshow');
-},  4000);
 
 });
